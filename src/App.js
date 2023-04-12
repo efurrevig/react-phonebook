ac3a97fda520da7e468edadeb428c4d34801c141
Eric Furrevig <efurrevig@gmail.com>
2023-04-12 19:47:50 -0400

    error/success messages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import personService from './services/persons'
 import Person from './components/Person'
 import Form from './components/Form'
 import Filter from './components/Filter'
-import Button from './components/Button'
+import Notification from './components/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,6 +11,8 @@ const App = () => {
   const [newNumber, setNewNumber] = useState('')
   const [shownPersons, setShownPersons] = useState([...persons])
   const [filter, setFilter] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
 
 
 
@@ -24,9 +26,24 @@ const App = () => {
   }, [])
 
   const deletePerson = (id) => {
-    personService.remove(id)
-    setPersons(persons.filter(person => person.id !== id))
-    setShownPersons(shownPersons.filter(person => person.id !== id))
+    personService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+        setShownPersons(shownPersons.filter(person => person.id !== id))
+      })
+      .then(() => {
+        setSuccessMessage(`Deleted ${persons.find(person => person.id === id).name}`)
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000)
+      })
+      .catch(error => {
+        setErrorMessage(`Information of ${persons.find(person => person.id === id).name} has already been removed from server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      })
   }
 
   const updatePerson = (id, personObject) => {
@@ -35,6 +52,12 @@ const App = () => {
       .then(returnedObject => {
         setPersons(persons.map(person => person.id !== id ? person : returnedObject))
         setShownPersons(shownPersons.map(person => person.id !== id ? person : returnedObject))
+      })
+      .then(() => {
+        setSuccessMessage(`Updated ${personObject.name}`)
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000)
       })
       .catch(error => {
         console.log(error.response.data)
@@ -66,6 +89,12 @@ const App = () => {
         setNewName('')
         setNewNumber('')
         setFilter('')
+      })
+      .then(() => {
+        setSuccessMessage(`Added ${personObject.name}`)
+        setTimeout(() => {
+          setSuccessMessage(null)
+        }, 5000)
       })
       .catch(error => {
         console.log(error.response.data)
@@ -107,6 +136,9 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} type="error" />
+      <Notification message={successMessage} type="success" />
+
       <div>
         <Filter filter={filter} onChange={handleFilterChange} />
       </div>
